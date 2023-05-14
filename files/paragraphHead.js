239ecb122d88acfca144e4873a97681a74c18140
H3S0Wq <H3S0Wq@cyber-dojo.org>
2023-05-14 12:27:29 +0000

    6 ran tests, predicted green, got green

--- files/paragraphHead.js
+++ files/paragraphHead.js
@@ -5,6 +5,8 @@ function paragraphHead(paragraph) {
   let lineHead = "On the ordinal day of Christmas";
   if (paragraph === 1) {
     ordinalNumber = "first";
+  } else if (paragraph === 2) {
+    ordinalNumber = "second";
   }
   lineHead = lineHead.replace("ordinal", ordinalNumber);
   return lineHead;
