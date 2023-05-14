d4c443397718105ed23068d154aff6f292a3e37b
H3S0Wq <H3S0Wq@cyber-dojo.org>
2023-05-14 12:18:21 +0000

    5 ran tests, predicted red, got red

--- files/paragraphHead.test.js
+++ files/paragraphHead.test.js
@@ -6,4 +6,7 @@ describe('Get the heading of the nth paragraph', () => {
   it('should return first day for paragraph one', () => {
     expect(paragraphHead(1)).toEqual('On the first day of Christmas');
   });
+  it('should return second day for paragraph two', () => {
+    expect(paragraphHead(2)).toEqual('On the second day of Christmas');
+  });
 });
