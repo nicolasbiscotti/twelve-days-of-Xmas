e9fb4b117da7d87bc4319bca308d775917c77044
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2023-05-23 10:53:24 -0300

    41 ran tests, predicted red, got red

--- files/giftDescription.test.js
+++ files/giftDescription.test.js
@@ -3,35 +3,38 @@
 const giftDescription = require("./giftDescription");
 
 describe("Get the description given the paragraph number and the gift number", () => {
-  it("should throw paragraph number must be greater or equal to one", () => {
+  it("should throw for Paragraph number less than one", () => {
     expect(() => giftDescription(0, 3)).toThrow(
       "Paragraph number must be greater than or equal to one"
     );
   });
-  it("should throw paragraph number must be less or equal to twelve", () => {
+  it("should throw for Paragraph number greater than twelve", () => {
     expect(() => giftDescription(13, 2)).toThrow(
       "Paragraph number must be less than or equal to twelve"
     );
   });
-  it("should throw gift number must be greater or equals one", () => {
+  it("should throw for Gift number less than one", () => {
     expect(() => giftDescription(1, 0)).toThrow(
       "Gift number must be greater than or equal to one"
     );
   });
-  it("should throw gift number must be less or equal to twelve", () => {
+  it("should throw for Gift number greater than twelve", () => {
     expect(() => giftDescription(1, 13)).toThrow(
       "Gift number must be less than or equal to twelve"
     );
   });
-  it("should return the first Gift", () => {
+  it("should return the first Gift description", () => {
     expect(giftDescription(1, 1)).toEqual("A partridge in a pear tree.");
   });
-  it("should return the first Gift", () => {
+  it("should return the first Gift description", () => {
     expect(giftDescription(12, 1)).toEqual("A partridge in a pear tree.");
   });
-  it("should throw Gift number cant't be ", () => {
+  it("should throw for Gift number greater than the Paragraph number", () => {
     expect(() => giftDescription(1, 2)).toThrow(
       "Gift number cannot be greater than the Paragraph number"
     );
+  });
+  it("should return the second Gift description", () => {
+    expect(giftDescription(2, 2)).toEqual("Two turtle doves and");
   });
 });
