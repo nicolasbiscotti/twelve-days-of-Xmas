1921d03cf37f58ddf0ac609f353f84002e11720a
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2023-05-23 10:03:28 -0300

    33 ran tests, predicted green, got red

--- files/giftDescription.js
+++ files/giftDescription.js
@@ -4,6 +4,9 @@ function giftDescription(paragraph, giftNumber) {
   if (paragraph < 1) {
     throw new Error("Paragraph number must be greater than or equal to one");
   }
+  if (paragraph > 12) {
+    throw new Error("Paragraph number must be greater than or equal to twelve");
+  }
   checkGiftNumberRange(giftNumber);
   return "A partridge in a pear tree.";
 }
