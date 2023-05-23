3755fd34934db9c692ea8e69ee674e97a745e09d
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2023-05-23 07:02:42 -0300

    26 ran tests, predicted green, got green

--- files/giftDescription.js
+++ files/giftDescription.js
@@ -4,6 +4,9 @@ function giftDescription(paragraph, giftNumber) {
   if (giftNumber < 1) {
     throw new Error("Gift number must be greater than or equals one");
   }
+  if (giftNumber > 12) {
+    throw new Error("Gift number must be less than or equal to twelve");
+  }
   return "A partridge in a pear tree.";
 }
 
