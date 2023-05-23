f0e86e525bdaf5b41877b2adfbc03012b337af8e
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2023-05-23 06:55:02 -0300

    23 ran tests, predicted red, got red

--- files/giftDescription.test.js
+++ files/giftDescription.test.js
@@ -6,4 +6,9 @@ describe("Get the description given the paragraph number and the gift number", (
   it("should return the first Gift", () => {
     expect(giftDescription(1, 1)).toEqual("A partridge in a pear tree.");
   });
+  it("should throw gift number must be greater or equals one", () => {
+    expect(() => giftDescription(1, 0)).toThrow(
+      "Gift number must be greater or equals one"
+    );
+  });
 });
