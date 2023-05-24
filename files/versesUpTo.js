65b1947a97400aec833503d6f6b81e9e6482e39b
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2023-05-24 17:10:00 -0300

    59 ran tests, predicted green, got green

--- files/versesUpTo.js
+++ files/versesUpTo.js
@@ -1,5 +1,19 @@
 function versesUpṬo(upTo, verseIntro, giftDescription) {
-  return verseIntro(upTo) + "\n" + giftDescription(upTo) + "\n";
+  let lyrics = "";
+  const range = [...Array(upTo).keys()].map((number) => number + 1);
+
+  let giftList = "";
+  for (const verseNumber of range) {
+    let verse = verseIntro(verseNumber) + "\n";
+    giftList = giftDescription(verseNumber) + "\n" + giftList;
+    if (lyrics === "") {
+      lyrics = verse + giftList;
+    } else {
+      lyrics = lyrics + "\n" + verse + giftList;
+    }
+  }
+
+  return lyrics;
 }
 
 module.exports = versesUpṬo;
