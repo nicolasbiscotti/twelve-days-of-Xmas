ab83f0e9e6fc74d67e176f721fabcae9c8c8a970
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2023-05-24 15:54:16 -0300

    54 ran tests, predicted red, got red

--- files/versesUpTo.test.js
+++ files/versesUpTo.test.js
@@ -3,11 +3,31 @@
 const versesUpṬo = require("./versesUpTo");
 
 describe("Get the first nth verses of the lyrics", () => {
-  it("should return only the firsty versse", () => {
+  it("should return only the firsty verse", () => {
     expect(versesUpṬo(1)).toEqual(
       "On the first day of Christmas" +
         "\n" +
         "My true love gave to me:" +
+        "\n" +
+        "A partridge in a pear tree." +
+        "\n"
+    );
+  });
+
+  it("should return firsty and second verses", () => {
+    expect(versesUpṬo(2)).toEqual(
+      "On the first day of Christmas" +
+        "\n" +
+        "My true love gave to me:" +
+        "\n" +
+        "A partridge in a pear tree." +
+        "\n" +
+        "\n" +
+        "On the second day of Christmas" +
+        "\n" +
+        "My true love gave to me:" +
+        "\n" +
+        "Two turtle doves and" +
         "\n" +
         "A partridge in a pear tree." +
         "\n"
