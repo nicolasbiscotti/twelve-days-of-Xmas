16d419feec6d2f45d731ea9db097780c97e26fd0
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2023-05-24 18:34:35 -0300

    69 ran tests, predicted green, got green

--- files/versesUpTo.test.js
+++ files/versesUpTo.test.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const versesUpTo = require("./versesUpTo");
+const realVerseIntro = require("./verseIntro");
+const realGiftDescription = require("./giftDescription");
 
 describe("Get the first nth verses of the lyrics", () => {
   function verseIntro(verseNumber) {
@@ -20,5 +22,9 @@ describe("Get the first nth verses of the lyrics", () => {
     expect(versesUpTo(2, verseIntro, giftDescription)).toEqual(
       "Intro 1\nGift 1\n\nIntro 2\nGift 2\nGift 1\n"
     );
+  });
+
+  it("should return the complete lyrics of 12-days-of-Xmas", () => {
+    console.log(versesUpTo(12, realVerseIntro, realGiftDescription));
   });
 });
