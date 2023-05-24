5b11b1b80749b7a05ae98f559e52f72764f9c115
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2023-05-24 16:19:27 -0300

    55 ran tests, predicted red, got red

--- files/versesUpTo.test.js
+++ files/versesUpTo.test.js
@@ -4,17 +4,18 @@ const versesUpṬo = require("./versesUpTo");
 
 describe("Get the first nth verses of the lyrics", () => {
   it("should return only the firsty verse", () => {
-    expect(versesUpṬo(1)).toEqual(
-      "On the first day of Christmas" +
-        "\n" +
-        "My true love gave to me:" +
-        "\n" +
-        "A partridge in a pear tree." +
-        "\n"
+    function verseIntro(verseNumber) {
+      return `Intro ${verseNumber}\n`;
+    }
+    function giftDescription(giftNumber) {
+      return `Gift ${giftNumber}`;
+    }
+    expect(versesUpṬo(1, verseIntro, giftDescription)).toEqual(
+      "Intro 1\nGift 1\n"
     );
   });
 
-  it("should return firsty and second verses", () => {
+  xit("should return firsty and second verses", () => {
     expect(versesUpṬo(2)).toEqual(
       "On the first day of Christmas" +
         "\n" +
