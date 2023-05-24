0fa0842aeef91cbdbcbc91e560660e2eb568509d
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2023-05-24 18:36:42 -0300

    70 ran tests, predicted red, got red

--- files/verseIntro.test.js
+++ files/verseIntro.test.js
@@ -4,13 +4,13 @@ const verseIntro = require("./verseIntro");
 
 describe("Get the introduction of the nth verse", () => {
   it("should return first day for verse one", () => {
-    expect(verseIntro(1)).toEqual("On the first day of Christmas\nMy true love gave to me:");
+    expect(verseIntro(1)).toEqual("On the first day of Christmas,\nMy true love gave to me:");
   });
   it("should return second day for verse two", () => {
-    expect(verseIntro(2)).toEqual("On the second day of Christmas\nMy true love gave to me:");
+    expect(verseIntro(2)).toEqual("On the second day of Christmas,\nMy true love gave to me:");
   });
   it("should return twelfth day for verse twelfth", () => {
-    expect(verseIntro(12)).toEqual("On the twelfth day of Christmas\nMy true love gave to me:");
+    expect(verseIntro(12)).toEqual("On the twelfth day of Christmas,\nMy true love gave to me:");
   });
   it("should throw an exception if no argument has been passed", () => {
     expect(verseIntro).toThrow("You must provide a verse number.");
