534c1021b3b03072e20de274fd145906458cfef3
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2023-05-24 15:45:41 -0300

    53 ran tests, predicted green, got green

--- files/versesUpTo.js
+++ files/versesUpTo.js
@@ -1,3 +1,8 @@
-function versesUpṬo(upTo) {}
+function versesUpṬo(upTo) {
+  let verseIntro = "On the first day of Christmas\nMy true love gave to me:\n";
+  let verseGiftList = "A partridge in a pear tree.\n";
+
+  return verseIntro + verseGiftList;
+}
 
 module.exports = versesUpṬo;
