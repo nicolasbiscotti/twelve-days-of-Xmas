fd03bd72d19925d911fea14d49a8319a9190c213
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2023-05-24 16:48:02 -0300

    58 ran tests, predicted red, got red

--- files/versesUpTo.test.js
+++ files/versesUpTo.test.js
@@ -3,35 +3,22 @@
 const versesUpṬo = require("./versesUpTo");
 
 describe("Get the first nth verses of the lyrics", () => {
+  function verseIntro(verseNumber) {
+    return `Intro ${verseNumber}`;
+  }
+  function giftDescription(giftNumber) {
+    return `Gift ${giftNumber}`;
+  }
+
   it("should return only the firsty verse", () => {
-    function verseIntro(verseNumber) {
-      return `Intro ${verseNumber}`;
-    }
-    function giftDescription(giftNumber) {
-      return `Gift ${giftNumber}`;
-    }
     expect(versesUpṬo(1, verseIntro, giftDescription)).toEqual(
       "Intro 1\nGift 1\n"
     );
   });
 
-  xit("should return firsty and second verses", () => {
-    expect(versesUpṬo(2)).toEqual(
-      "On the first day of Christmas" +
-        "\n" +
-        "My true love gave to me:" +
-        "\n" +
-        "A partridge in a pear tree." +
-        "\n" +
-        "\n" +
-        "On the second day of Christmas" +
-        "\n" +
-        "My true love gave to me:" +
-        "\n" +
-        "Two turtle doves and" +
-        "\n" +
-        "A partridge in a pear tree." +
-        "\n"
+  it("should return firsty and second verses", () => {
+    expect(versesUpṬo(2, verseIntro, giftDescription)).toEqual(
+      "Intro 1\nGift 1\n\nIntro 2\nGift 2\nGift 1\n"
     );
   });
 });
